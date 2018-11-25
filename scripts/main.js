536e75cf483a1be562a99f5034562563a01f9ffb
Stanis1St <sogenokotowary@yandex.ru>
2018-11-26 01:58:23 +0300

    Update 2018-11-25T22:58:19.000Z

--- scripts/main.js
+++ scripts/main.js
@@ -85,6 +85,7 @@ mySlider();
 
 
 ////аккордеон тим
+
 $(function(){
     let $wraps=$('.team-acco__content-wrap');
 	$('.team-acco__trigger').on('click', function(e){
@@ -97,4 +98,111 @@ $(function(){
 	});
 	
 
+
+   
+
+/// overlay 
+const openButton = document.querySelector("#openOverlay");
+const successOverlay = createOverlay("Привет, <b>loftschool</b>!");
+console.log(openButton);
+openButton.addEventListener("click", function() {
+    document.body.appendChild(successOverlay);
+  });
+  
+
+  function createOverlay(content) {
+    const overlayElement = document.createElement("div");
+    overlayElement.classList.add("overlay");
+  
+    const template = document.querySelector("#overlayTemplate");
+    overlayElement.innerHTML = template.innerHTML;
+  
+    const closeElement = overlayElement.querySelector(".close");
+    closeElement.addEventListener("click", function() {
+      document.body.removeChild(overlayElement);
+    });
+  
+    const contentElement = overlayElement.querySelector(".content");
+    contentElement.innerHTML = content;
+  
+    return overlayElement;
+  }
+
 });
+
+
+///acccordeon menu
+
+
+function accordionMenu (){
+    const menuItems = document.querySelectorAll('.menu__item');
+    const menuAccord = document.querySelector('.menu__accordion');
+    menuAccord.addEventListener('click',event => {
+        event.preventDefault();
+        let target =event.target.parentNode;
+        let content =target.nextElementSibling;
+        let item =target.parentNode;
+
+        const tarWidth=target.clientWidth;
+        const windowWidth =document.documentElement.clientWidth;
+        const layoutContentWidth =520;
+        const breakpointPhone = 480 ;
+        const closeMenuWidth= tarWidth *menuItems.length;
+        const openMenuWidth = closeMenuWidth +layoutContentWidth;
+
+        if(event.target.classList.contains('menu__title')){
+            moveMenu();
+
+        }
+        
+        target=event.target
+        content=target.nextElementSibling;
+        item=target.parentNode;
+        
+        
+        if (target.classList.contains('menu__link')){
+            moveMenu();
+
+        }
+
+
+        function moveMenu (){
+            for (const iterator of menuItems) {
+                if (iterator !=item){
+                    console.log(iterator);
+
+                    iterator.classList.remove('is-active');
+                   iterator.lastElementChild.style.width = 0;
+                   menuAccord.style.transform= `translateX(0)`;
+                }
+            }
+
+            if (item.classList.contains('is-active')){
+             item.classList.remove('is-active');
+               content.style.width = 0 ;
+            }else  {
+                item.classList.add('is-active');
+                content.style.width = layoutContentWidth + 'px';
+                if (windowWidth > breakpointPhone && windowWidth < openMenuWidth) {
+                  content.style.width = windowWidth - closeMenuWidth + 'px';
+                } else if (windowWidth <= breakpointPhone) {
+                  let num;
+                  for (let i = 0; i < menuItems.length; i++) {
+                    if (menuItems[i] === item) {
+                      num = menuItems.length - (i + 1);
+                    }
+                    menuAccord.style.transform = `translateX(${tarWidth * num}px)`;
+                    content.style.width = windowWidth - tarWidth + 'px';
+                  }
+                } else {
+                  content.style.width = layoutContentWidth + 'px';
+                }
+              }
+
+
+        }
+
+    })
+}
+
+accordionMenu();
